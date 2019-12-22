fc9d1464bfa72e4bbfebf9c56eeb39dd27fa1cb1
Yazeed Algobaisi <algobaisi.yazeed@hotmail.com>
2019-12-22 22:51:13 +0300

    added connection settings

---
+++ DM.ModuleChat/ViewModels/ConnectionSettingsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using DM.ModuleChat.Services;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DM.ModuleChat.ViewModels
+{
+    public class ConnectionSettingsViewModel : BindableBase
+    {
+        private readonly IConnectionSettingsService connectionSettings;
+
+        public ConnectionSettingsViewModel(IConnectionSettingsService connectionSettings)
+        {
+            this.connectionSettings = connectionSettings;
+            this.connectionSettings.UserName = Environment.UserName;
+        }
+
+        public IConnectionSettingsService ConnectionSettings { get { return connectionSettings; } }
+    }
+}
